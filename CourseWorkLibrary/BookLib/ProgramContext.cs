c2eac2d70d38e5c4c5208347db70a14cb9c61eff
KirillVenzhik <kirilvenzhik@gmail.com>
2023-06-21 01:12:53 +0300

    Reupload_Proj

---
+++ CourseWorkLibrary/BookLib/ProgramContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Program;
+using System;
+using System.ComponentModel.Design;
+
+namespace CourseWorkLibrary.BookLib
+{
+    public class ProgramContext : DbContext
+    {
+        public ProgramContext()
+        {
+        
+        }
+
+        public ProgramContext(DbContextOptions<ProgramContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ CourseWorkLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CourseWorkLibrary;
+using CourseWorkLibrary.BookLib;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace Program
+{
+    class Library
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(70, 50);
+            Console.SetBufferSize(70, 50);
+
+            string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\kiril\\OneDrive\\Рабочий стол\\CourseWorks\\CourseWorkLibrary\\CourseWorkLibrary\\LibDb.mdf\";Integrated Security=True";
+            var options = new DbContextOptionsBuilder<ProgramContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+            using var db = new ProgramContext(options);
+            db.Database.EnsureCreated();
+
+            var controller = new MenuFunctions(db);
+            controller.Introduction();
+        }
+    }
+}
